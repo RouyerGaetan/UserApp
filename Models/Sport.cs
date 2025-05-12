259122eb47a233db4c7a23d2531cca2057a23274
Gaetan Rouyer <gaetan.rouyer@hotmail.fr>
2025-05-12 22:50:19 +0200

    Clean up migrations, keep only InitialCreate and CleanMigration

--- Models/Sport.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserApp.Models
-{
-    public class Sport
-    {
-        public int Id { get; set; } // Identifiant unique du sport
-        public string? Name { get; set; } // Nom du sport
-    }
-}
