967804fa80a1745ba88a68ca0dbf7783b016ef09
Gaetan Rouyer <gaetan.rouyer@hotmail.fr>
2025-05-22 09:01:10 +0200

    BugFix

---
+++ wwwroot/js/alert.js
@@ -0,0 +1,10 @@
+﻿function autoCloseAlert(timeout = 3000) {
+    window.setTimeout(function () {
+        const alert = document.querySelector('.alert');
+        if (alert) {
+            alert.classList.remove('show');
+            alert.classList.add('hide');
+            setTimeout(() => alert.remove(), 500);
+        }
+    }, timeout);
+}
